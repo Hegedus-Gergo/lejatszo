e01d8a1ca704c52167a872f94f6769b28d9db744
Hegedus-Gergo <71663811+Hegedus-Gergo@users.noreply.github.com>
2024-05-22 08:10:55 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using NAudio.Wave;
+
+namespace AudioPlayer
+{
+    public partial class MainWindow : Window
+    {
+        private WaveStream hang;
+        private WaveOutEvent waveout;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mediaElement_Loaded(object sender, RoutedEventArgs e)
+        {
+            hang = new AudioFileReader("file.mp3");
+            waveout = new WaveOutEvent();
+            waveout.Init(hang);
+            waveout.Play();
+            kivalasztott.Play();
+            lejatszasbtn.IsEnabled = false;
+            megallitbtn.IsEnabled = true;
+            leallitbtn.IsEnabled = true;
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            waveout.Play();
+            kivalasztott.Play();
+            lejatszasbtn.IsEnabled = false;
+            megallitbtn.IsEnabled = true;
+            leallitbtn.IsEnabled = true;
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            waveout.Pause();
+            kivalasztott.Pause();
+            lejatszasbtn.IsEnabled = true;
+            megallitbtn.IsEnabled = false;
+            leallitbtn.IsEnabled = true;
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            waveout.Stop();
+            kivalasztott.Stop();
+            lejatszasbtn.IsEnabled = true;
+            megallitbtn.IsEnabled = false;
+            leallitbtn.IsEnabled = false;
+        }
+
+        private void mediaElement_Vegetert(object sender, RoutedEventArgs e)
+        {
+            lejatszasbtn.IsEnabled = true;
+            megallitbtn.IsEnabled = false;
+            leallitbtn.IsEnabled = false;
+        }
+    }
+}
